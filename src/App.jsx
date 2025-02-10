3de3181033e16e27f3048919afd9ec71653f51ed
Subrata Guchhait <sguchhaitdev@gmail.com>
2025-02-10 20:29:17 +0530

    Adding Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
-import Body from "./components/Body";
-import Navbar from "./components/Navbar";
+// import Body from "./components/Body";
+// import Navbar from "./components/Navbar";
+import HomePage from "./pages/home";
 
 function App() {
+  const appRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <HomePage />,
+    },
+  ]);
   return (
     <>
-      <div className="m-36 mt-0 mb-0">
-        <Navbar />
-        <Body />
-      </div>
+      <RouterProvider router={appRouter} />
     </>
   );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Body from "../components/Body";
+import Navbar from "../components/Navbar";
+
+const HomePage = () => {
+  return (
+    <div className="m-36 mt-0 mb-0">
+      <Navbar />
+      <Body />
+    </div>
+  );
+};
+
+export default HomePage;
